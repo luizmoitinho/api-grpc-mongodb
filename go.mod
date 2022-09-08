f1720e5269b709badaefb4f42d72c2410799245c
luizmoitinho <luiz.moitinho@olxbr.com>
2022-09-07 23:05:22 -0300

    chore: adding proto structure and docker-compose file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luizmoitinho/api-grpc-mongodb
+
+go 1.18
+
+require go.mongodb.org/mongo-driver v1.10.2
---
+++ server/blog_item.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	pb "github.com/luizmoitinho/api-grpc-mongodb/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorId string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
+func documentToBlog(data *BlogItem) *pb.Blog {
+
+}
